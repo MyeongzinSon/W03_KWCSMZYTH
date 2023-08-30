49af3af37c6fabfe5b6ee7e3616b68d2bf034e65
손명진 <sonmyungzin1@gmail.com>
2023-08-30 09:55:29 +0900

    (*) 코드의 클래스, 메서드 이름을 직관적으로 변경

---
+++ Assets/IInputListener.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInputListener
+{
+    public virtual void UpdateInput(InputButton inputButton)
+    {
+        switch (inputButton)
+        {
+            case InputButton.U: UpdateUp(); return;
+            case InputButton.D: UpdateDown(); return;
+            case InputButton.R: UpdateRight(); return;
+            case InputButton.L: UpdateLeft(); return;
+            case InputButton.A: UpdateA(); return;
+            case InputButton.B: UpdateB(); return;
+            default: return;
+        }
+
+    }
+    public abstract void UpdateUp();
+    public abstract void UpdateDown();
+    public abstract void UpdateRight();
+    public abstract void UpdateLeft();
+    public abstract void UpdateA();
+    public abstract void UpdateB();
+}
